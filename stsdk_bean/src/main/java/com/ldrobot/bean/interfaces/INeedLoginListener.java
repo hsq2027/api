a6ba4bf0faa9c82fae5b6e8a29c61eed081c8257
Stewart <1182921546@qq.com>
2024-05-11 17:47:26 +0800

    1、修改包名

--- stsdk_bean/src/main/java/com/ldrobot/bean/interfaces/INeedLoginListener.java
+++
@@ -1,7 +0,0 @@
-package com.ldrobot.bean.interfaces;
-
-import android.content.Context;
-
-public interface INeedLoginListener {
-    void onNeedLogin(Context context);
-}
--- stsdk_utils/src/main/java/com/ldrobot/utils/util/netconfig/ble/BleCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.ldrobot.utils.util.netconfig.ble;
-
-public interface BleCallBack {
-    void onConnectState(int state); // state 1：断开连接  2:需要重连  3.连接失败  4.可以发数据
-
-    void onReceiver(String msg);
-}
